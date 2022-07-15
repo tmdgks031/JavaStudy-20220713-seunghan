93b22af4b54222a83d9e1656119cd3a9eb72c11a
tmdgks031 <tmdgks031@naver.com>
2022-07-15 11:05:48 +0900

    챕터 1 출력

---
+++ src/j01_출력/Myinfo.java
@@ -0,0 +1,26 @@
+package j01_출력;
+
+public class Myinfo {
+
+	public static void main(String[] args) {
+		char name1 = '신';
+		char name2 = '승';
+		char name3 = '한';
+		String name = "신승한";
+		
+		int age = 27;
+		
+		String address = "부산 부산진구 전포동";
+		
+		System.out.print("이름: ");
+		System.out.println("" + name1 + name2 + name3);
+		System.out.print("나이: ");
+		System.out.println(age);
+		System.out.print("주소: ");
+		System.out.println(address);
+		System.out.println();
+		
+		
+	}
+
+}
