a899e48c5e7326b2237ecb20dfdcc7730446bf18
tmdgks031 <tmdgks031@naver.com>
2022-07-15 11:06:55 +0900

    챕터3 연산자

---
+++ src/j03_연산자/Operation1.java
@@ -0,0 +1,26 @@
+package j03_연산자;
+
+/*
+ * ctrl + space -> 자동완성
+ */
+
+public class Operation1 {
+
+	public static void main(String[] args) {
+		int num = 50;
+		
+		System.out.println(++num);		
+		System.out.println(num);		
+		System.out.println(num++);
+		System.out.println(num);
+		
+		System.out.println(--num);		
+		System.out.println(num);		
+		System.out.println(num--);
+		System.out.println(num);
+		
+		
+		
+	}
+
+}
